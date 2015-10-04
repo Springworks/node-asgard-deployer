109cc065b346014edae4940e14f08b56ec4e8c8f
Kristofer Sommestad <kristofer.sommestad@springworks.se>
2015-10-04 22:01:57 +0200

    Implemented waitUntilDeploymentComplete

--- test/unit/deployer-test.js
+++ test/unit/deployer-test.js
@@ -18,6 +18,51 @@ describe(__filename, () => {
     asgard_service_instance = asgard_service.create('http://localhost:3000', 'eu-west-1');
   });
 
+  describe('internals.waitUntilDeploymentComplete', () => {
+
+    describe('with valid asgard_client, deployment_id', () => {
+      const deployment_id = 42;
+      const status_check_interval_ms = 10;
+
+      describe('when deployment property goes from false -> true after 1 call', () => {
+        let get_deployment_stub;
+
+        beforeEach(() => {
+          get_deployment_stub = sinon_sandbox.stub(asgard_service_instance, 'getDeployment');
+        });
+
+        beforeEach(function mockFirstCallToBeNotDone() {
+          const fixture = asgard_fixtures.getDeployment();
+          fixture.done = false;
+          get_deployment_stub.onCall(0).returns(Promise.resolve(fixture));
+        });
+
+        beforeEach(function mockSecondCallToBeDone() {
+          const fixture = asgard_fixtures.getDeployment();
+          fixture.done = true;
+          get_deployment_stub.onCall(1).returns(Promise.resolve(fixture));
+        });
+
+        it('should resolve promise', () => {
+          return deployer
+              .internals.waitUntilDeploymentComplete(asgard_service_instance, status_check_interval_ms, deployment_id)
+              .should.be.fulfilled();
+        });
+
+        it('should check deployment status twice', () => {
+          return deployer
+              .internals.waitUntilDeploymentComplete(asgard_service_instance, status_check_interval_ms, deployment_id)
+              .then(() => {
+                get_deployment_stub.getCalls().should.have.length(2);
+              });
+        });
+
+      });
+
+    });
+
+  });
+
   describe('internals.isDeploymentComplete', () => {
 
     describe('with valid asgard_client, deployment_id', () => {
