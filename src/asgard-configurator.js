0023204155f1c56743937fd568e717c6b4609555
Kristofer Sommestad <kristofer.sommestad@springworks.se>
2015-10-13 15:12:42 +0200

    Validating env var config

---
+++ src/asgard-configurator.js
@@ -0,0 +1,38 @@
+const input_validator = require('@springworks/input-validator');
+const joi = input_validator.joi;
+
+const internals = {
+  env_vars_validation_schema: joi.object().required().keys({
+    NODE_ASGARD_DEPLOYER_ASGARD_HOST: joi.string().required(),
+    NODE_ASGARD_DEPLOYER_ASGARD_PASSWORD: joi.string().required(),
+    NODE_ASGARD_DEPLOYER_ASGARD_USERNAME: joi.string().required(),
+    NODE_ASGARD_DEPLOYER_AWS_REGION: joi.string().required(),
+  }),
+};
+
+exports.createConfigFromEnvVars = function(env_vars) {
+  const validated = input_validator.validateSchema(env_vars, internals.env_vars_validation_schema);
+  const config = {
+    host: validated.NODE_ASGARD_DEPLOYER_ASGARD_HOST,
+    aws_region: validated.NODE_ASGARD_DEPLOYER_AWS_REGION,
+  };
+
+  const basic_auth = internals.parseBasicAuth(validated);
+  if (basic_auth) {
+    config.basic_auth = basic_auth;
+  }
+
+  return config;
+};
+
+
+internals.parseBasicAuth = function(validated_env_vars) {
+  if (validated_env_vars.NODE_ASGARD_DEPLOYER_ASGARD_USERNAME && validated_env_vars.NODE_ASGARD_DEPLOYER_ASGARD_PASSWORD) {
+    return {
+      username: validated_env_vars.NODE_ASGARD_DEPLOYER_ASGARD_USERNAME,
+      password: validated_env_vars.NODE_ASGARD_DEPLOYER_ASGARD_PASSWORD,
+    };
+  }
+  return null;
+};
+
